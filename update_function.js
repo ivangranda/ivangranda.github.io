7f438ab084efd62e0e86ab447c38be448b4b337c
ivangranda <ivangranda93@gmail.com>
2016-05-19 17:18:30 +0200

    V6.1
    Problemas de actualizacion solucionados

--- update_function.js
+++ update_function.js
@@ -1,25 +1,25 @@
 function update()
 				{
 				    trans=900;
-				    extentY_1 = d3.extent(data_PIB,function(d){return +d[country]})
-				    extentY_2 = d3.extent(data_PIB,function(d){return +d[country_2]})
+				    extentY_1 = d3.extent(data_ID,function(d){return +d[country]})
+				    extentY_2 = d3.extent(data_ID,function(d){return +d[country_2]})
 				    y.domain([d3.min([extentY_1[0],extentY_2[0]]),d3.max([extentY_1[1],extentY_2[1]])])
 
 
 				    //yAxis.scale(y)
-				    d3.select('#PIB_graph').selectAll('.y.axis')
+				    d3.select('#ID_graph').selectAll('.y.axis')
 				        .transition().duration(trans)
 				        .call(yAxis)
-				    d3.select('#PIB_graph').select('#line_1')
+				    d3.select('#ID_graph').select('#line_1')
 				        .transition()
 				        .duration(trans)
-				        .attr('d',valueline_1(data_PIB))
+				        .attr('d',valueline_1(data_ID))
 
 
-				    d3.select('#PIB_graph').select('#line_2')
+				    d3.select('#ID_graph').select('#line_2')
 				    .transition()
 				        .duration(trans)
-				        .attr('d',valueline_2(data_PIB))
+				        .attr('d',valueline_2(data_ID))
 				        
 
 				   //extentY = d3.extent(data_inv,function(d){return +d[country]})
