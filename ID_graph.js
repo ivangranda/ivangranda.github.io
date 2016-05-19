0e9af9bc48a2fa1ede955d68eeb7cb632c81af43
ivangranda <ivangranda93@gmail.com>
2016-05-19 17:17:31 +0200

    V7
    Indicador tSNE añadido

---
+++ ID_graph.js
@@ -0,0 +1,96 @@
+var data_ID;
+var margin = {top: 10, right: 00, bottom: 50, left: 30},
+    width = 300 - margin.left - margin.right,
+    height = 200 - margin.top - margin.bottom;
+
+    // Set the ranges
+    var x = d3.scale.linear().range([0, width]);
+    var y = d3.scale.linear().range([height, 0]);
+
+    var xAxis = d3.svg.axis().scale(x)
+        .orient("bottom").ticks(8)
+        .innerTickSize(-height)
+        .outerTickSize(0);
+
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left").ticks(8)
+        .innerTickSize(-width)
+        .outerTickSize(0);
+        
+
+    var valueline_1 = d3.svg.line()
+        .x(function(d) { return x(d['']); })
+        .y(function(d) { return y(d[country]); });
+
+    var valueline_2 = d3.svg.line()
+        .x(function(d) { return x(d['']); })
+        .y(function(d) { 
+            if (country_2==null){
+                return y(d[country]);
+            }
+            else{
+            return y(d[country_2]); }
+        })
+
+    // Gráfico de PIB
+    d3.csv('datos/indicador_TSNE_mundial.csv', function(d) {
+        data_ID=d;
+
+
+        x.domain(d3.extent(data_ID,function(d){return +d['']}));
+        y.domain(d3.extent(data_ID,function(d){return +d['ESP']}));
+
+        // Adds the svg canvas
+        var ID_graph = d3.select("#ID_graph")
+            .append("svg")
+            .attr("x",0)
+            .attr("y",0)
+            .attr("id","ID_graph")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+
+            .append("g")
+            .attr("transform",
+                "translate(" + margin.left + "," + margin.top + ")");
+
+        ID_graph.append("path")
+            .attr("class","line")
+            .attr("id","line_2")
+            .attr("d",valueline_2(d))
+            .on('mouseover',function(d){
+                d3.select(this)
+                    .style("stroke-width",5)})
+            .on('mouseout',function(d){
+                d3.select(this)
+                    .style("stroke-width",2)});
+            
+        ID_graph.append("path")
+            .attr("class","line")
+            .attr("id","line_1")
+            .attr("d",valueline_1(d))
+            .on('mouseover',function(d){
+                d3.select(this)
+                    .style("stroke-width",5)})
+            .on('mouseout',function(d){
+                d3.select(this)
+                    .style("stroke-width",2)});
+
+
+
+        // Add the X Axis
+        ID_graph.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis)
+            .selectAll("text")  
+            .style("text-anchor", "end")
+            .attr("dx", "-.8em")
+            .attr("dy", ".15em")
+            .attr("transform", "rotate(-65)" );
+
+        // Add the Y Axis
+       ID_graph.append("g")
+            .attr("class", "y axis")
+            .call(yAxis);
+            });
